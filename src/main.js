f749584f3779bbb101b5a1495822b86275ace788
Maryna Sibova <maryna.i.kipenko@gmail.com>
2025-03-11 14:33:30 -0400

    tasks created

---
+++ src/main.js
@@ -0,0 +1,7 @@
+import { createGallery } from './js/1-gallery.js';
+import { handleForm } from './js/2-form.js';
+
+document.addEventListener('DOMContentLoaded', () => {
+  createGallery();
+  handleForm();
+});
